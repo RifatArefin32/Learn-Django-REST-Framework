47ebe24fed0e647281c9da1e995cf12560687e07
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-11-15 19:18:39 +0600

    Create view method for listing all products

--- mcommerce/product/views.py
+++ mcommerce/product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+# list all the products
+def list_products(request):
+    products = Product.objects.all()
+    serialized_products = ProductSerializer(products)
+    context = {
+        'products': serialized_products.data
+    }
+    return Response(context)
