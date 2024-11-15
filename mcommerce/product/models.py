a91db5933b71188ccfd4f200f8489b1ab15c0c9c
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-11-15 19:05:23 +0600

    Create Product model

--- mcommerce/product/models.py
+++ mcommerce/product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    cost = models.DecimalField(decimal_places=2, max_digits=6)
+    expire_date = models.DateField(null=True, blank=True)
+    description = models.TextField()
