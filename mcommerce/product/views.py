5cf600d3eace523526a5c9f4273b236c01e13b8e
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-11-15 19:30:57 +0600

    Add decorators `api_view`

--- mcommerce/product/views.py
+++ mcommerce/product/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
 
 # list all the products
+@api_view(['GET'])
 def list_products(request):
     products = Product.objects.all()
     serialized_products = ProductSerializer(products, many=True)
