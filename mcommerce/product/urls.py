559c3024f4602f07d001c3e3cb7444a529368da3
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-11-18 15:06:06 +0600

    Update url to add product view set url

--- mcommerce/product/urls.py
+++ mcommerce/product/urls.py
@@ -2,6 +2,16 @@ from django.urls import path, include
 from .views import list_products, list_messages
 from .views import ClassProducts, ClassProductDetails
 from .views import GenericProducts, GenericProductDetails, GenericSpecialProductDetails
+from .views import ProductViewsets
+from rest_framework.routers import DefaultRouter
+
+
+
+router = DefaultRouter()
+router.register(
+    'productviewset', ProductViewsets, basename='product'
+)
+
 
 urlpatterns = [
     path('product-list/', list_products, name='ListProduct'), # DRF authentication
@@ -14,6 +24,5 @@ urlpatterns = [
     # generic view
     path('generic-product-list/', GenericProducts.as_view(), name='generic_products'),
     path('generic-product-detail/<int:pk>/', GenericProductDetails.as_view(), name='generic_product_details'), # note: generic url expects 'pk' as query parament
-    path('generic-special-product-detail/<int:pk>/', GenericSpecialProductDetails.as_view(), name='generic_special_product_details'), # note: generic url expects 'pk' as query parament
-    
-]
+    path('generic-special-product-detail/<int:pk>/', GenericSpecialProductDetails.as_view(), name='generic_special_product_details'), # note: generic url expects 'pk' as query parament 
+] + router.urls
