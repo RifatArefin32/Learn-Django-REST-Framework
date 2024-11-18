ba973caba356bde9c5be50b334bb78eeef674397
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-11-18 14:39:12 +0600

    Add url to show all products using generics product list view

--- mcommerce/product/urls.py
+++ mcommerce/product/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from .views import list_products, list_messages
 from .views import ClassProducts, ClassProductDetails
+from .views import GenericsProducts
 
 urlpatterns = [
     path('product-list/', list_products, name='ListProduct'), # DRF authentication
     path('message-list/', list_messages, name='ListMessage'), 
     path('class-product-list/', ClassProducts.as_view(), name='class_products'),
     path('class-product-detail/<int:id>/', ClassProductDetails.as_view(), name='class_product_details'),
-    
+    path('generics-product-list/', GenericsProducts.as_view(), name='generics_products'),
 ]
