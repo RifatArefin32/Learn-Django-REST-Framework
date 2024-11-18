b786e1e251c619b271e1570ab6b7957d651f35ab
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-11-18 13:46:12 +0600

    view method for creating a product with APIview (Class based View)

--- mcommerce/product/views.py
+++ mcommerce/product/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Product, Message
 from .serializers import ProductSerializer, MessageSerializer
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+
 
 # function based views
 # list all the products (function based view)
@@ -57,9 +59,21 @@ class ClassProducts(APIView):
         serializer_class = ProductSerializer(products, many=True)
         return Response(serializer_class.data)
     
+    def post(self, request):
+        serialized_data = ProductSerializer(data=request.data)
+        if serialized_data.is_valid(raise_exception=True):
+            product_saved = serialized_data.save()
+            context = {
+                "message": "Product {} is created successfully".format(product_saved.name),
+            }
+            return Response(context, status=status.HTTP_201_CREATED)
+        return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+# Views for product details
 class ClassProductDetails(APIView):
     def get(self, request, id):
         products = Product.objects.filter(id=id)
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+    
