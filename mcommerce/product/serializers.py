155e9a61b7797eee5d83d4ea4da4f2d9b6e1ba5b
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-11-18 11:50:19 +0600

    Simple serializer `MessageSerializer` created

--- mcommerce/product/serializers.py
+++ mcommerce/product/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils import timezone
 from .models import Product
 
 # model based serializer
@@ -8,3 +9,11 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
         # fields = ['name', 'price']
         # Note: `model` and `fields` are predefined keywords for model serializer
+        
+
+# simple serializer (does not depend on model)
+# serializer for message
+class MessageSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    content = serializers.CharField(max_length=200)
+    created_at = serializers.DateTimeField(default=timezone.now) 
