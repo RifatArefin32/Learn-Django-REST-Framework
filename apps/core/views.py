15a89da2e6eca526760d6a2384af61b5336fc5b3
RifatArefin32 <arefinrifat786@gmail.com>
2025-05-12 08:53:51 +0600

    Create view for list of products

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+
+    return JsonResponse({
+        "data": serializer.data
+    })
