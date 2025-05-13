3b6f0a60f6921a7edee487885c5b42800f3ab9a5
RifatArefin32 <arefinrifat786@gmail.com>
2025-05-13 08:51:23 +0600

    Create nested serializer for Orders

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Product, Order, OrderItem
 
-
+# product serializer
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -12,7 +12,7 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'stock'
         )
-    
+
     # custom field level validation
     def validate_price(self, value):
         if value <= 0:
@@ -20,3 +20,18 @@ class ProductSerializer(serializers.ModelSerializer):
                 "Price must be greater than zero"
             )
         return value
+
+
+# order item serializer
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity']
+
+
+# order serializer (nested serializer)
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = Order
+        fields = ['order_id', 'user', 'created_at', 'status', 'items']
